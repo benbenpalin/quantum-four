73c7ab2f119002de7a3a5d245e4f7a073e09ee22
benpalin1@gmail.com <benpalin1@gmail.com>
2017-11-14 20:58:52 -0600

    moved to the next available space in the column, from the bottom up, when player clicks a space

--- src/cljs/quantum_four/events.cljs
+++ src/cljs/quantum_four/events.cljs
@@ -7,9 +7,6 @@
  (fn  [_ _]
    db/default-db))
 
-;(rf/reg-event-fx
-;  ::player-move)
-
 (rf/reg-event-db
   ::change-turn
   (fn [db _]
@@ -17,9 +14,16 @@
           new-turn (if (= old-turn :r) :b :r)]
       (assoc db :turn new-turn))))
 
+(defn find-empty-space [[orig-row column] board] ;;TODO make an alert if row is full instead of just forcing in the original value
+  (loop [row 6]
+    (if (>= row 0)
+      (if (not= :e (get-in board [row column]))
+        (recur (dec row))
+        [row column])
+      [orig-row column])))
 
 (rf/reg-event-fx
   ::select-column
   (fn [{:keys [db]} [_ space]]
-    {:db (update db :board #(assoc-in % space (:turn db)))
+    {:db (update db :board #(assoc-in % (find-empty-space space (:board db)) (:turn db)))
      :dispatch [::change-turn]}))
